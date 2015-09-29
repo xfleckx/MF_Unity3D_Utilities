ca45aad8f6b14b214399fc11b64b50cad786c3c0
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-09-30 00:05:56 +0200

    [FEATURE] Add alignment target selection

--- Editor/CustomObjectAlignment.cs
+++ Editor/CustomObjectAlignment.cs
@@ -6,6 +6,8 @@ using System.Linq;
 
 public class CustomObjectAlignment : EditorWindow {
 
+    private enum AlignmentTypes { Origin, FirstSelected, LastSelected }
+
     private const string WindowTitle = "Alignment Tools";
     
     [MenuItem("Window/MF_CustomAlignment")]
@@ -44,21 +46,42 @@ public class CustomObjectAlignment : EditorWindow {
 
         return false;
     }
+      
+    private AlignmentTypes targetAlignment = AlignmentTypes.Origin;
 
     private void RenderAlignmentsAtOrigin()
     {
         EditorGUILayout.LabelField("Align Objects at Origin on:");
 
+
+
+        targetAlignment = (AlignmentTypes) EditorGUILayout.EnumPopup(targetAlignment);
+
         EditorGUILayout.BeginHorizontal();
 
         if (GUILayout.Button("X") && hasSelection)
         {
-                var transforms = selecteObjects.Select((o) => o.transform);
+            var transforms = selecteObjects.Select((o) => o.transform);
 
-                foreach (var item in transforms)
+            foreach (var item in transforms)
+            {
+                switch (targetAlignment)
                 {
-                    item.position = new Vector3(0, item.position.y, item.position.z);
+                    case AlignmentTypes.Origin:
+                        item.position = new Vector3(0, item.position.y, item.position.z);
+                        break;
+                    case AlignmentTypes.FirstSelected:
+                        var first = transforms.First().position;
+                        item.position = new Vector3(first.x, item.position.y, item.position.z);
+                        break;
+                    case AlignmentTypes.LastSelected:
+                        var last = transforms.Last().position;
+                        item.position = new Vector3(last.x, item.position.y, item.position.z);
+                        break;
+                    default:
+                        break;
                 }
+            }
         }
 
         if (GUILayout.Button("Y") && hasSelection)
@@ -67,7 +90,22 @@ public class CustomObjectAlignment : EditorWindow {
 
                 foreach (var item in transforms)
                 {
-                    item.position = new Vector3(item.position.x, 0, item.position.z);
+                    switch (targetAlignment)
+                    {
+                        case AlignmentTypes.Origin:
+                            item.position = new Vector3(item.position.x, 0, item.position.z);
+                            break;
+                        case AlignmentTypes.FirstSelected:
+                            var first = transforms.First().position;
+                            item.position = new Vector3(item.position.y, first.y, item.position.z);
+                            break;
+                        case AlignmentTypes.LastSelected:
+                            var last = transforms.Last().position;
+                            item.position = new Vector3(item.position.x, last.y, item.position.z);
+                            break;
+                        default:
+                            break;
+                    } 
                 }
         }
 
@@ -77,12 +115,27 @@ public class CustomObjectAlignment : EditorWindow {
 
             foreach (var item in transforms)
             {
-                item.position = new Vector3(item.position.x, item.position.y, 0);
+                switch (targetAlignment)
+                {
+                    case AlignmentTypes.Origin:
+                        item.position = new Vector3(item.position.x, item.position.z, 0);
+                        break;
+                    case AlignmentTypes.FirstSelected:
+                        var first = transforms.First().position;
+                        item.position = new Vector3(item.position.y, item.position.y, first.z);
+                        break;
+                    case AlignmentTypes.LastSelected:
+                        var last = transforms.Last().position;
+                        item.position = new Vector3(item.position.x, item.position.y, last.z);
+                        break;
+                    default:
+                        break;
+                } 
             } 
         }
 
         EditorGUILayout.EndHorizontal();
 
     }
-    
+     
 }
